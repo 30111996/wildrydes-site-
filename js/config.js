d133a38a64972747268d5690f44e88723184cd15
Sandeep <Sandeepmalakar9@gmail>
2022-05-27 17:31:51 +0530

    changed in config.js file

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_a2i4t654a', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ntt2mmqdf1at47cob7ir1gq61', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
